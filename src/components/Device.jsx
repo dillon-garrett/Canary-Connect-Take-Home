9dac200bea07ceaeb5ffc6567ccead30c20799db
Sally Sheimo <sallysheimo@Sallys-MacBook-Pro-2.local>
2019-10-16 14:02:34 -0400

    new library graphing

--- src/components/Device.jsx
+++ src/components/Device.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { XYPlot, LineSeries } from 'react-vis';
+import { VictoryBar, VictoryChart, VictoryAxis } from 'victory';
 
 const Device = props => {
   const { text, id, proxy } = props;
@@ -28,20 +29,37 @@ const Device = props => {
       });
   };
 
-  const data = [];
+  const humidData = [];
   for (let i = 0; i < readings.humidity.length - 1; i += 1) {
-    const obj = {};
-    obj[i] = readings.humidity[i];
-    data.push(obj);
+    const obj = { index: 0, humidity: 0 };
+    obj.index = i;
+    obj.humidity = readings.humidity[i];
+    humidData.push(obj);
   }
+
+  const xTick = [...Array(humidData.length).keys()];
+  const data = [
+    { quarter: 1, earnings: 13000 },
+    { quarter: 2, earnings: 16500 },
+    { quarter: 3, earnings: 14250 },
+    { quarter: 4, earnings: 19000 }
+  ];
   return (
     <section className="device" id={id}>
       {text}
       <button type="button" className="get-graph-data" onClick={handleClick}>
         Click Me
       </button>
-      <XYPlot height={300} width={300} />
-      <LineSeries data={data} />
+      <VictoryChart domainPadding={20}>
+        <VictoryAxis
+          tickValues={[1, 2, 3, 4]}
+          tickFormat={['Quarter 1', 'Quarter 2', 'Quarter 3', 'Quarter 4']}
+        />
+        <VictoryAxis dependentAxis tickFormat={x => `$${x / 1000}k`} />
+        <VictoryBar data={data} x="quarter" y="earnings" />
+      </VictoryChart>
+      {/* <XYPlot height={300} width={300} />
+      <LineSeries data={data} /> */}
     </section>
   );
 };
