0fa097b3239c167763bb3cde2931a0b43001ccb5
Sally Sheimo <sallysheimo@Sallys-MacBook-Pro-2.local>
2019-10-16 13:28:18 -0400

    graphs

--- src/components/Device.jsx
+++ src/components/Device.jsx
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { XYPlot, LineSeries } from 'react-vis';
 
 const Device = props => {
   const { text, id, proxy } = props;
   const url = `https://fullstack-challenge-api.herokuapp.com/devices/${id}/readings`;
-  const readingsObj = { humidity: [], temperature: [], airquality: [] };
+  const sample = { humidity: [], temperature: [], airquality: [] };
+  const [readings, setReadings] = useState(sample);
 
   const handleClick = () => {
     fetch(proxy + url, {
@@ -13,23 +15,33 @@ const Device = props => {
     })
       .then(res => res.json())
       .then(response => {
+        const readingsObj = { humidity: [], temperature: [], airquality: [] };
         response.forEach(el => {
           if (el.type === 'humidity') readingsObj.humidity.push(el.value);
           if (el.type === 'temperature') readingsObj.temperature.push(el.value);
           if (el.type === 'airquality') readingsObj.airquality.push(el.value);
         });
-        console.log(readingsObj, 'this is readingsObj');
+        setReadings(readingsObj);
       })
       .catch(() => {
         throw new Error('error in fetching device readings');
       });
   };
+
+  const data = [];
+  for (let i = 0; i < readings.humidity.length - 1; i += 1) {
+    const obj = {};
+    obj[i] = readings.humidity[i];
+    data.push(obj);
+  }
   return (
     <section className="device" id={id}>
       {text}
       <button type="button" className="get-graph-data" onClick={handleClick}>
         Click Me
       </button>
+      <XYPlot height={300} width={300} />
+      <LineSeries data={data} />
     </section>
   );
 };
